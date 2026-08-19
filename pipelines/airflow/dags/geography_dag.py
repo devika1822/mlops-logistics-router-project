e60ed0f17c595e782d6defdf9a389305ebb4c3d1
Devika Dileep <devikadileep2018@gmail.com>
2026-08-19 10:22:09 +0530

    Automate geography model training with Airflow

--- pipelines/airflow/dags/geography_dag.py
+++ pipelines/airflow/dags/geography_dag.py
@@ -5,9 +5,11 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
 
+PROJECT_PATH = r"C:\Users\HP\mlops-logistics-router-project"
+
 with DAG(
     dag_id="geography_pipeline",
-    description="Geography track preprocessing pipeline",
+    description="Geography track preprocessing and model training pipeline",
     start_date=datetime(2026, 8, 18),
     schedule=None,
     catchup=False,
@@ -29,9 +31,31 @@ with DAG(
         mount_tmp_dir=False,
         mounts=[
             Mount(
-                source=r"C:\Users\HP\mlops-logistics-router-project",
+                source=PROJECT_PATH,
                 target="/opt/project",
                 type="bind",
             )
         ],
     )
+
+    train_final_model = DockerOperator(
+        task_id="train_final_model",
+        image="geography-trainer:latest",
+        command="python /opt/project/tracks/geography/train_final.py",
+        docker_url="unix://var/run/docker.sock",
+        network_mode="mlops-logistics-router-project_default",
+        environment={
+            "MLFLOW_TRACKING_URI": "http://mlflow:5000",
+        },
+        auto_remove=True,
+        mount_tmp_dir=False,
+        mounts=[
+            Mount(
+                source=PROJECT_PATH,
+                target="/opt/project",
+                type="bind",
+            )
+        ],
+    )
+
+    spark_preprocessing >> train_final_model
